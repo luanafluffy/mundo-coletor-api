34ba2b1c570eff743ab272385c3638578561f79e
Luana <8068324-Carvalho21@users.noreply.gitlab.com>
2022-06-10 07:19:01 -0300

    Refatoração

--- app/src/Entity/Collector.php
+++ app/src/Entity/Collector.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,11 @@ class Collector
      */
     public ?string $phoneNumberCall = null;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=CollectionMaterial::class, inversedBy="collectors")
+     */
+    private $collection_material;
+
     public function __construct(
         string $mainName,
         int $fetchCollection,
@@ -46,5 +53,30 @@ class Collector
         $this->fetchCollection = $fetchCollection;
         $this->receiveCollection = $receiveCollection;
         $this->phoneNumberCall = $phoneNumberCall;
+        $this->collection_material = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, CollectionMaterial>
+     */
+    public function getCollectionMaterial(): Collection
+    {
+        return $this->collection_material;
+    }
+
+    public function addCollectionMaterial(CollectionMaterial $collectionMaterial): self
+    {
+        if (!$this->collection_material->contains($collectionMaterial)) {
+            $this->collection_material[] = $collectionMaterial;
+        }
+
+        return $this;
+    }
+
+    public function removeCollectionMaterial(CollectionMaterial $collectionMaterial): self
+    {
+        $this->collection_material->removeElement($collectionMaterial);
+
+        return $this;
     }
 }
